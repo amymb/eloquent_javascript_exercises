53c8e063205d5bfab33f8e701a010f61e57751f1
Amy Bertken <amy.bertken@gmail.com>
2015-04-19 13:11:01 -0600

    looping triangle 5 ways

---
+++ looping_triangle.js
@@ -0,0 +1,31 @@
+var pound = []
+for(i = 0; i < 7; i++){
+  pound.push("#");
+  console.log(pound.join(''))
+}
+
+var pound = []
+var i = 0
+while (i<7){
+  pound.push("#");
+  console.log(pound.join(''));
+  i+=1
+}
+
+// the following solutions came after I looked at the eloquent javascript website solution
+// I was thinking too much of string concatenation, but the .length thing is better   
+
+var pound = "#"
+while(pound.length < 8){
+ console.log(pound);
+ pound += "#";
+}
+
+for(pound = "#"; pound.length < 8; pound+= "#")
+  console.log(pound);
+
+var pound = "#"
+do{
+  console.log(pound)
+  pound +="#"
+} while(pound.length <8);
