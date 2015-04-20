0bdfce75c8552128259393950b306f6fc9791cab
Amy Bertken <amy.bertken@gmail.com>
2015-04-20 15:19:20 -0600

    write minimum of 2 function

--- chess_board.js
+++ chess_board.js
@@ -4,17 +4,19 @@ function chessBoard(size){
   for(var i = 0; i < size; ++i){
     var pound1 = "";
     var pound2 = "";
+    var combo ="";
     if (i%2==0){
       while (pound1.length < size) {
           pound1 += "# ";
       }
-      console.log(pound1);
+      combo += (pound1);
     }else{
       while (pound2.length < size){
         pound2 += " #";
       }
-      console.log(pound2);
+      combo += (pound2);
     }
+    console.log(combo)
   }
 }
 
---
+++ minimum.js
@@ -0,0 +1,7 @@
+var min = function (x, y){
+  if (x < y){
+    return x;
+  }else{
+    return y;
+  };
+};
