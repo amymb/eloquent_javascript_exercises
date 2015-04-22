4ca3757bf8695361798750815a803a4f1e87f90b
Amy Bertken <amy.bertken@gmail.com>
2015-04-21 19:17:04 -0600

    refactor fizzbuzz, chess_board, create sum of range

--- chess_board.js
+++ chess_board.js
@@ -16,27 +16,29 @@ function chessBoard(size){
       }
       combo += (pound2);
     }
-    console.log(combo)
+    console.log(combo);
   }
 }
 
- chessBoard(8)
+ chessBoard(8);
+
+
 
 
 function chessBoard2(size){
-  for(var i = 0; i < size; ++i){
-  if (i%2==0){
-    for(var pound1 = "# "; pound1.length<size; pound1+="# "){
-      pound1
-      }
-  console.log(pound1)
+  var i;
+  var pound1;
+  var pound2;
+
+  for(i = 0; i < size; ++i){
+    if (i%2==0){
+      for(pound1 = "# "; pound1.length < size; pound1 += "# "){}
+    console.log(pound1);
     }else{
-    for(var pound2 = " #"; pound2.length<size; pound2+=" #"){
-      pound2
-      }
-  console.log(pound2)
+      for(pound2 = " #"; pound2.length < size; pound2 += " #"){}
+    console.log(pound2);
     }
   }
 }
 
-chessBoard2(24)
+chessBoard2(24);
---
+++ sum_of_range.js
@@ -0,0 +1,35 @@
+var range = function(start, end){
+  var array = [];
+  var i;
+  for(i=start; i<=end; i++){
+    array.push(i);
+  };
+  return array;
+};
+
+var sum = function(array){
+  var total = 0;
+  var i;
+  for(i=0; i< array.length; i++){
+    total += array[i];
+  };
+  return total;
+};
+
+\\bonus!
+
+var betterRange = function(start, end, step){
+  var array = [];
+  var i;
+  function steps(step){
+    if (step === undefined){
+      i++;
+    }else{
+      i+=step;
+    };
+  };
+  for(i = start; i<=end; steps(step)){
+    array.push(i);
+  };
+  return array;
+};
